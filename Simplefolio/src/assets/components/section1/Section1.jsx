bfb3505c078d73786cf81d114afcfb37d5a0cde3
VictordalaM <delamotte.v@gamil.com>
2024-01-16 15:53:46 +0100

    css in progress

--- Simplefolio/src/assets/components/section1/Section1.jsx
+++ Simplefolio/src/assets/components/section1/Section1.jsx
@@ -1,10 +1,14 @@
+import './Section1.css'
+
 const Section1 = () => {
     return ( 
         <>
-        <h1>Hi, I am John Smith</h1>
-        <h2>A Front End Developer</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum tenetur iusto laboriosam optio dolore, voluptate distinctio in at excepturi iste inventore, natus ad odit minus quidem, numquam modi ducimus placeat?</p>
-        <a href="#">resume</a>
+        <div id="section1">
+            <h1>Hi, I am John Smith</h1>
+            <h2>A Front End Developer</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum tenetur iusto laboriosam optio dolore, voluptate distinctio in at excepturi iste inventore, natus ad odit minus quidem, numquam modi ducimus placeat?</p>
+            <a href="#" id='resumeButton'>resume</a>
+        </div>
     </>
     );
     
--- Simplefolio/src/assets/components/section2/Section2.jsx
+++ Simplefolio/src/assets/components/section2/Section2.jsx
@@ -1,21 +1,27 @@
+import './Section2.css'
+
 const Section2 = () => {
     return ( 
         <>
-        <h2>PROJECTS</h2>
-        <div className="projects">
-            <h3>Project 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, quisquam.</p>
-            <p>JavaScript React Sass</p>
-        </div>
-        <div className="projects">
-            <h3>Project 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, quisquam.</p>
-            <p>JavaScript React Sass</p>
-        </div>
-        <div className="projects">
-            <h3>Project 3</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, quisquam.</p>
-            <p>JavaScript React Sass</p>
+        <div id='section2'>
+            <h2>PROJECTS</h2>
+            <div id='projectBox'>
+                <div className="projects">
+                    <h3>Project 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, quisquam.</p>
+                    <p>JavaScript React Sass</p>
+                </div>
+                <div className="projects">
+                    <h3>Project 2</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, quisquam.</p>
+                    <p>JavaScript React Sass</p>
+                </div>
+                <div className="projects">
+                    <h3>Project 3</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, quisquam.</p>
+                    <p>JavaScript React Sass</p>
+                </div>
+            </div>
         </div>
     </>
     );
